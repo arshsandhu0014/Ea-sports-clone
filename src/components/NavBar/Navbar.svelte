aa44c052cbe64a5aaca107cb45532f068474367d
Arshsandhu <arshsandhu0014@gmail.com>
2023-06-02 18:11:57 +0530

    bug fixed

--- src/components/NavBar/Navbar.svelte
+++ src/components/NavBar/Navbar.svelte
@@ -132,8 +132,9 @@
     display: none;
     position: absolute;
     background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    border-radius: .75rem;
+    min-width: 200px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.9);
     z-index: 1;
   }
 
